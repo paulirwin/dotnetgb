6f4870d9cd4cdddcc3e7e7b9b7080ed56daa711a
Paul Irwin <paulirwin@gmail.com>
2020-09-19 15:27:32 -0600

    Rename logo

--- DotNetGB/Hardware/Cartridges/Mbc1.cs
+++ DotNetGB/Hardware/Cartridges/Mbc1.cs
@@ -5,7 +5,7 @@ namespace DotNetGB.Hardware.Cartridges
 {
     public class Mbc1 : IAddressSpace
     {
-        private static readonly int[] NINTENDO_LOGO = {
+        private static readonly int[] LOGO = {
             0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D,
             0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99,
             0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E
@@ -216,9 +216,9 @@ namespace DotNetGB.Hardware.Cartridges
             for (int i = 0; i < rom.Length; i += 0x4000)
             {
                 bool logoMatches = true;
-                for (int j = 0; j < NINTENDO_LOGO.Length; j++)
+                for (int j = 0; j < LOGO.Length; j++)
                 {
-                    if (rom[i + 0x104 + j] != NINTENDO_LOGO[j])
+                    if (rom[i + 0x104 + j] != LOGO[j])
                     {
                         logoMatches = false;
                         break;
